8f44a28c4d9cca70f94bcc20d839f2facb41b31e
unknown <abbasalizada252@gmail.com>
2025-02-25 16:27:04 +0430

    settled for server

--- Frontend/src/Components/dashboard/dashboardChart/AdminMainPage.jsx
+++ Frontend/src/Components/dashboard/dashboardChart/AdminMainPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BiBed, BiBookAdd, BiGroup, BiHome } from "react-icons/bi";
 import { FaAmbulance, FaChevronDown, FaHeartbeat } from "react-icons/fa";
 import { FaClipboardUser } from "react-icons/fa6";
+import { GiCoins } from "react-icons/gi";
 const AdminMainPage = () => {
   const overviewdata = [
     {
@@ -11,23 +12,23 @@ const AdminMainPage = () => {
     },
     {
       title: "تعداد  دیزاینر ",
-      number: "3,454",
+      number: 233,
       icon: <FaClipboardUser size={24} className="text-violet-600" />,
     },
     {
-      title: "Avg Treat, Costs",
-      number: "$2,054",
-      icon: <BiBookAdd size={24} className="text-orange-500" />,
+      title: "باقی مانده",
+      number: "2345234",
+      icon: <GiCoins size={24} className="text-yellow-500" />,
     },
     {
-      title: "Available Cars",
-      number: 34,
-      icon: <FaAmbulance size={24} className="text-pink-800" />,
+      title: "دریافتی",
+      number: 845745245,
+      icon: <GiCoins size={24} className="text-yellow-500" />,
     },
     {
-      title: "Available Cars",
-      number: 34,
-      icon: <FaAmbulance size={24} className="text-pink-800" />,
+      title: "مجموعی",
+      number: 234523523,
+      icon: <GiCoins size={24} className="text-yellow-500" />,
     },
   ];
   return (
@@ -40,7 +41,7 @@ const AdminMainPage = () => {
             className="h-[100px] xl:h-[120px] bg-white shadow-md rounded-lg px-10 py-4 flex justify-between items-center"
           >
             <div className="flex items-center justify-center gap-x-7">
-              <span className="p-3 rounded-full bg-gray-200">{item.icon}</span>
+              <span className="p-3 rounded-full border">{item.icon}</span>
               <div>
                 <span className="text-xl font-bold">{item.number}</span>
                 <p className="font-semibold text-gray-700">{item.title}</p>
