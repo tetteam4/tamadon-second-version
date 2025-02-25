9d2721dab583477a1aeb28c3a87fc5dc8258745f
unknown <abbasalizada252@gmail.com>
2025-02-25 10:21:38 +0430

    structure of charts for each user is created

---
+++ Frontend/src/Components/dashboard/dashboardChart/ordersCamparisonChart.jsx
@@ -0,0 +1,31 @@
+import {
+  BarChart,
+  Bar,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const OrdersComparisonChart = ({ chartData }) => {
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <BarChart data={chartData}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="completed" fill="#8884d8" />
+        <Bar dataKey="processing" fill="#82ca9d" />
+        <Bar dataKey="taken" fill="#FFBB28" />
+        <Bar dataKey="pending" fill="#FF8042" />
+        <Bar dataKey="delivered" fill="#AF19FF" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default OrdersComparisonChart;
