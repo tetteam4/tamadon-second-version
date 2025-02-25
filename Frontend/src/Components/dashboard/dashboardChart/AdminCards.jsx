5d7155f3abcaa07351cbd557cce8c22483a98ef6
unknown <abbasalizada252@gmail.com>
2025-02-25 10:21:38 +0430

    structure of charts for each user is created

---
+++ Frontend/src/Components/dashboard/dashboardChart/AdminCards.jsx
@@ -0,0 +1,9 @@
+const AdminCards = ({}) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6 mb-8">
+      {" "}
+    </div>
+  );
+};
+
+export default AdminCards;
