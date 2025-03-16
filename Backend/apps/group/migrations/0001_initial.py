5f7a6f871158c3e2761effef5cfae0c0d9a3cef6
AukTo1380 <auktoafg@gmail.com>
2025-03-16 16:04:53 +0430

    no changes

---
+++ Backend/apps/group/migrations/0001_initial.py
@@ -0,0 +1,91 @@
+
+import apps.group.models
+import django.db.models.deletion
+import django_jalali.db.models
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=255)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('role', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'verbose_name': 'Category',
+                'verbose_name_plural': 'Categories',
+            },
+        ),
+        migrations.CreateModel(
+            name='AttributeType',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=50)),
+                ('attribute_type', models.CharField(choices=[('dropdown', 'dropdown'), ('date', 'date'), ('checkbox', 'checkbox'), ('input', 'input')], default='select attribute type', max_length=100)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('category', models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='attribute_types', to='group.category')),
+            ],
+            options={
+                'unique_together': {('name', 'category')},
+            },
+        ),
+        migrations.CreateModel(
+            name='Order',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('order_name', models.CharField(max_length=255)),
+                ('customer_name', models.CharField(max_length=255)),
+                ('secret_key', models.CharField(default=apps.group.models.generate_secret_key, editable=False, max_length=6, unique=True)),
+                ('status', models.CharField(choices=[('pending', 'Pending'), ('taken', 'Taken'), ('processing', 'Processing'), ('done', 'Done'), ('delivered', 'Delivered')], default='pending', max_length=50)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('attributes', models.JSONField(blank=True, default=dict, null=True)),
+                ('category', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='group.category')),
+                ('designer', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='orders', to=settings.AUTH_USER_MODEL)),
+                ('printer', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='printer_orders', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'verbose_name': 'Order',
+                'verbose_name_plural': 'Orders',
+            },
+        ),
+        migrations.CreateModel(
+            name='ReceptionOrder',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('price', models.DecimalField(blank=True, decimal_places=2, max_digits=10, null=True)),
+                ('receive_price', models.DecimalField(blank=True, decimal_places=2, max_digits=10, null=True)),
+                ('reminder_price', models.DecimalField(blank=True, decimal_places=2, max_digits=10, null=True)),
+                ('delivery_date', django_jalali.db.models.jDateField()),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('order', models.OneToOneField(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='group.order')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='AttributeValue',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('attribute_value', models.CharField(max_length=255)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('attribute', models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='attribute_values', to='group.attributetype')),
+            ],
+            options={
+                'unique_together': {('attribute', 'attribute_value')},
+            },
+        ),
+    ]
