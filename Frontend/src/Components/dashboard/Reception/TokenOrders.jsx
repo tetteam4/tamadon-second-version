45d8e58ea7c04e8e30d60c7dc5145af416d2eb28
unknown <abbasalizada252@gmail.com>
2025-03-23 01:20:51 +0430

    time interval fetching of data in designer past order page is removed

--- Frontend/src/Components/dashboard/Reception/TokenOrders.jsx
+++ Frontend/src/Components/dashboard/Reception/TokenOrders.jsx
@@ -51,19 +51,6 @@ const TokenOrders = () => {
       return null;
     }
   };
-  const status = [
-    { id: 1, name: "Designer" },
-    { id: 2, name: "Reception" },
-    // { id: 0, name: "Admin" },
-    { id: 3, name: "Head of designers" },
-    { id: 4, name: "Printer" },
-    { id: 5, name: "Delivery Agent" },
-    { id: 6, name: "Digital" },
-    { id: 7, name: "Bill" },
-    { id: 8, name: "Chaspak" },
-    { id: 9, name: "Shop role" },
-    { id: 10, name: "Laser" },
-  ];
   const toggleOrderSelection = (neworder) => {
     setSelectedOrders((prevSelected) =>
       prevSelected.includes(neworder)
